0ff527bbbdac4d7b20dea7d4ea14829e190bcfd1
JiWoo <huynhztienzdat@gmail.com>
2019-11-05 23:36:13 +0700

    select range

--- src/lib/dashboard/components/dashboard/dashboard.component.ts
+++ src/lib/dashboard/components/dashboard/dashboard.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
+import { FormControl } from '@angular/forms';
 
-import * as MOCK from '../mock/mock';
+import { Observable } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
+
 import { DashboardService } from '../../store/dashboard.service';
 
 @Component({
@@ -11,6 +14,14 @@ import { DashboardService } from '../../store/dashboard.service';
 })
 export class DashboardComponent implements OnInit {
 
+  locationCtrl = new FormControl();
+  filteredLocations: Observable<any>;
+  ranges = [
+    { id: 1, value: '1 day' },
+    { id: 7, value: '7 days' },
+    { id: 30, value: '30 days' },
+  ];
+
   constructor(
     public dashboardService: DashboardService,
     private dialog: MatDialog
@@ -18,6 +29,21 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
     this.dashboardService.getData();
+
+    this.filteredLocations = this.locationCtrl.valueChanges.pipe(
+      startWith(''),
+      map(name => name ? this.filterLocation(name) : this.dashboardService.locations)
+    );
+  }
+
+  filterLocation(value: string) {
+    const search = value.toLowerCase();
+    return this.dashboardService.locations.filter(d => d.toLowerCase().indexOf(search) === 0);
+
+  }
+
+  onSelectRange(id: number) {
+    console.log('range = ', id);
   }
 
 }
